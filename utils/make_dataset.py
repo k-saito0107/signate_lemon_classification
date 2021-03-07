679a03d6daaacafb3f1c56598705526e0adc356d
k-saito <1902kei0107>
2021-03-08 00:32:27 +0900

    first push

--- utils/make_dataset.py
+++ utils/make_dataset.py
@@ -24,6 +24,7 @@ class Make_Dataset(data.Dataset):
 
         img_name = img_file_path.split('/')[-1]
         label = self.label_dict[img_name]
+        print(label)
 
         return img, label
     
